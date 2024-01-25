fa6aaa1ad9926ec072ea7a1b0c91a098a692e1f8
Aditya <adityanagare09@gmail.com>
2024-01-25 16:52:34 +0530

    changed deloy to vercel

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize"
 
-const redirectUri = "https://65b23fd0ec6d7e55f6edcece--jade-custard-9ae5b9.netlify.app/"
+const redirectUri = "https://spotify-app-eosin-pi.vercel.app/"
 
 const clientId = "beeae12f468c49cdab3e0b4c54add10f"
 
